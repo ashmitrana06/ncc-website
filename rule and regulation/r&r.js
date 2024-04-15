8d57d7df3c87ac7fbb961b8bf4e47b0698d666fd
ashmitrana06 <167062968+ashmitrana06@users.noreply.github.com>
2024-04-15 12:48:19 +0530

    Add files via upload

---
+++ rule and regulation/r&r.js
@@ -0,0 +1,8 @@
+function turnOnOverlay() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function turnOffOverlay() {
+    document.getElementById("overlay").style.display = "none";
+  }
+  
